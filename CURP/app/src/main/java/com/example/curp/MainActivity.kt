c9b686d1d0d177aa359fa5fd2ff3b13d11c2db75
root <root@Hayabusa.localdomain>
2021-07-13 12:56:12 -0500

    Corrigiendo los estilos

--- CURP/app/src/main/java/com/example/curp/MainActivity.kt
+++ CURP/app/src/main/java/com/example/curp/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         var sexo = ""
         val valores = mutableListOf("0","1","2","3","4","5","6","7","8","9")
--- RFC/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ RFC/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         val btnGenerate = findViewById<Button>(R.id.generar)
         val btnBorrar = findViewById<Button>(R.id.borrar)
